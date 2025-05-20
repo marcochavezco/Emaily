725d8d38167d55b2daae58953703d7e4198c86ef
marcochavezco <machavezco@gmail.com>
2025-05-20 17:39:38 -0600

    setup project

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,20 @@
+import ThemeSwitcher from '@/components/ThemeSwitcher';
+import { UserButton } from '@clerk/nextjs';
+import React from 'react';
+
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className='flex flex-col min-h-screen min-w-full bg-background max-h-screen'>
+      <nav className='flex items-center justify-between p-4 bg-background border-b border-border'>
+        <div className='text-lg font-bold'>e-maker</div>
+        <div className='flex items-center space-x-4'>
+          <ThemeSwitcher />
+          <UserButton afterSwitchSessionUrl='/sign-in' />
+        </div>
+      </nav>
+      <main className='flex w-full flex-grow'>{children}</main>
+    </div>
+  );
+}
+
+export default Layout;
