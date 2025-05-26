48c3d21a3110f956e5c7e330be94c527818bd91c
marcochavezco <machavezco@gmail.com>
2025-05-26 13:20:55 -0600

    rename project

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -6,7 +6,7 @@ function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className='flex flex-col min-h-screen min-w-full bg-background max-h-screen'>
       <nav className='flex items-center justify-between p-4 bg-background border-b border-border'>
-        <div className='text-lg font-bold'>e-maker</div>
+        <div className='text-lg font-bold'>Emaily</div>
         <div className='flex items-center space-x-4'>
           <ThemeSwitcher />
           <UserButton afterSwitchSessionUrl='/sign-in' />
